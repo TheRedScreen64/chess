728ffed69e8ff86a94dc4c2bb6207ec21c6c4d3d
TheRedScreen <theredscreen@t-online.de>
2024-05-14 10:29:29 +0200

    Add init functions

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -4,5 +4,40 @@ using namespace std;
 
 int main()
 {
-    
+    static char board[8][8] = {
+        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+    };
+
+    Color color = selectColor();
+}
+
+Color selectColor() {
+    Color color;
+    bool colorSelected = false;
+    while (!colorSelected) {
+        color = promptForColor();
+        if (color != White && color != Black) {
+            system("cls");
+            continue;
+        }
+        colorSelected = true;
+    }
+    system("cls");
+    return color;
+}
+
+Color promptForColor() {
+    Color color;
+    int tempNumber;
+    cout << "Which color do you want to play? (Black 0, White 1)";
+    cin >> tempNumber;
+    color = (Color)tempNumber;
+    return color;
 }
--- ChessGame.h
+++ ChessGame.h
@@ -3,3 +3,11 @@
 #include <iostream>
 #include <string>
 
+enum Color {
+    Black,
+    White
+};
+
+void displayBoard();
+
+Color promptForColor();
